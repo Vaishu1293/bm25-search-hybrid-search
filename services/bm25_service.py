c2e93eb174c17492278d452e93c272e922dd8e48
Vaishali Shivakumar <130084250+Vaishu1293@users.noreply.github.com>
2026-08-16 17:51:58 +0100

    bm25-index

---
+++ services/bm25_service.py
@@ -0,0 +1,13 @@
+from rank_bm25 import BM25Okapi
+
+def create_bm25_index(documents):
+    tokenized_documents = []
+
+    for document in documents:
+        # split() returns a list of tokens directly
+        text = document["text"].lower().split()
+        tokenized_documents.append(text)
+
+    bm25Index = BM25Okapi(tokenized_documents)
+    
+    return bm25Index
